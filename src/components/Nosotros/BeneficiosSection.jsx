4230bfce2b93c505e137476c365a3afb0690ddfe
Santiagoservera <santiagoservera21@gmail.com>
2025-04-14 11:22:31 -0300

    cambio de imagen conflictiva

--- src/components/Nosotros/BeneficiosSection.jsx
+++ src/components/Nosotros/BeneficiosSection.jsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-import ahorroCostos from "../../assets/AhorroCostos.jpg";
 
 const Beneficios = [
   {
@@ -11,42 +10,48 @@ const Beneficios = [
     nombre: "Ahorra costos",
     descripcion:
       "Al tercerizar, las empresas evitan costos asociados a la contratación, capacitación, y gestión del personal de limpieza, así como la compra y mantenimiento de equipos y suministros de limpieza.",
-    imagen: ahorroCostos,
+    imagen:
+      "https://images.pexels.com/photos/5805492/pexels-photo-5805492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 2,
     nombre: "Especialización y Calidad",
     descripcion:
       "Las empresas de limpieza profesional cuentan con personal capacitado y experimentado en técnicas de limpieza avanzadas, garantizando resultados de alta calidad y un entorno más limpio y saludable.",
-    imagen: ahorroCostos,
+    imagen:
+      "https://images.pexels.com/photos/5805492/pexels-photo-5805492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 3,
     nombre: "Enfoque en el Core Business",
     descripcion:
       "Delegar la limpieza a una empresa especializada permite que la empresa se concentre en su actividad principal, mejorando la eficiencia y productividad.",
-    imagen: ahorroCostos,
+    imagen:
+      "https://images.pexels.com/photos/5805492/pexels-photo-5805492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 4,
     nombre: "Flexibilidad y Adaptabilidad",
     descripcion:
       "Las empresas de limpieza pueden adaptar sus servicios a las necesidades específicas del cliente, ofreciendo horarios y frecuencias de limpieza flexibles que se ajusten a la operación del negocio.",
-    imagen: ahorroCostos,
+    imagen:
+      "https://images.pexels.com/photos/5805492/pexels-photo-5805492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 5,
     nombre: "Cumplimiento Normativo",
     descripcion:
       "Las empresas de limpieza profesional están al tanto de las regulaciones y normativas en materia de higiene y seguridad, asegurando que todos los procedimientos cumplan con los estándares legales vigentes.",
-    imagen: ahorroCostos,
+    imagen:
+      "https://images.pexels.com/photos/5805492/pexels-photo-5805492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: 6,
     nombre: "Reducción de Riesgos y Responsabilidades",
     descripcion:
       "Las empresas de limpieza profesional cuentan con personal capacitado y experimentado en técnicas de limpieza avanzadas, garantizando resultados de alta calidad y un entorno más limpio y saludable.",
-    imagen: ahorroCostos,
+    imagen:
+      "https://images.pexels.com/photos/5805492/pexels-photo-5805492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
 

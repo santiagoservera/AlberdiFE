8d934b627b31b7df4d4dc50aa74752e1ff0f2c11
Santiagoservera <santiagoservera21@gmail.com>
2025-05-02 13:28:19 -0300

    Imagen beneficios fix

--- src/components/Nosotros/BeneficiosSection.jsx
+++ src/components/Nosotros/BeneficiosSection.jsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-import ahorroCostos from "../../assets/AhorroCostos.jpg";
 
 const Beneficios = [
   {

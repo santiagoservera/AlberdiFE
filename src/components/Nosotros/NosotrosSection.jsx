99a6d619172c589444e3b544b3e6943d2540687c
Santiagoservera <santiagoservera21@gmail.com>
2025-05-02 13:59:10 -0300

    cambiando imagenes a png

--- src/components/Nosotros/NosotrosSection.jsx
+++ src/components/Nosotros/NosotrosSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Nosotros from "../../assets/nosotros.jpg";
+import Nosotros from "../../assets/nosotros.png";
 import DivisorHome from "../Home/DivisorHome";
 
 const NosotrosSection = () => {
